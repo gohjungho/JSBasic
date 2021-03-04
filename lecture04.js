503dccc070d97c0d7755ed0e3ae0a546e7af2e82
gohjungho <46920818+gohjungho@users.noreply.github.com>
2021-03-04 17:53:00 +0900

    Add files via upload

--- lecture04.js
+++ lecture04.js
@@ -1,2 +1,2 @@
-var name = prompt("이름을 입력해 주세요: ");
-console.log(name,"님 환영합니다.");
+var name = prompt("이름을 입력해 주세요:");
+console.log(name,"님 환영합니다. ");
---
+++ lecture05.js
@@ -0,0 +1,7 @@
+var n = 1;
+var b = true;
+var s = "문자열 입니다.";
+
+console.log(n, typeof(n));
+console.log(b, typeof(b));
+console.log(s, typeof(s));
